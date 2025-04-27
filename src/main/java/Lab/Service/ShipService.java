679946d4fb12fc793ee6bbdd050a7ad28acbdc68
RazaDon Willrich <razradical93@gmail.com>
2025-04-27 02:55:17 +0000

    Completed Spring Transactional.

--- src/main/java/Lab/Service/ShipService.java
+++ src/main/java/Lab/Service/ShipService.java
@@ -4,7 +4,10 @@ import Lab.Exceptions.InvalidTonnageException;
 import Lab.Model.Ship;
 import Lab.Repository.ShipRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
@@ -19,7 +22,9 @@ import java.util.List;
  * attempting to get all ships. No ships should be persisted if any ship in the array has a negative or zero
  * tonnage - we're left to assume some form of unwanted user error in that case.
  */
+
 @Service
+@Transactional(rollbackFor = InvalidTonnageException.class)
 public class ShipService {
     ShipRepository shipRepository;
     @Autowired
@@ -32,6 +37,7 @@ public class ShipService {
      * @param ships transient ship entities
      * @throws InvalidTonnageException ships can not have negative tonnage (they'd sink)
      */
+    
     public List<Ship> addListShips(List<Ship> ships) throws InvalidTonnageException {
         List<Ship> persistedShips = new ArrayList<>();
         for(int i = 0; i < ships.size(); i++){
@@ -45,12 +51,14 @@ public class ShipService {
     /**
      * @return all ship entities
      */
+    
     public List<Ship> getAllShips() {
         return shipRepository.findAll();
     }
     /**
      * @return ship entity by id
      */
+    
     public Ship getShipById(long id) {
         return shipRepository.findById(id).get();
     }
